ca6b601e420850ce07fffcbdf39af5c901afc287
SEOKJUHYUN <seokjuhyun@naver.com>
2021-12-27 23:18:54 +0900

    FIFO, LIFO, Priority 3가지 구조의 Queue

--- queue/queue.py
+++ queue/queue.py
@@ -7,7 +7,7 @@ class QueueClass :
         self.queue : list = []
 
     def enqueue(self, insert_value : Union[int, str, tuple[int, Any]]) -> None :
-        """Queue 데이터를 입력하는 함수.
+        """Queue 데이터를 입력하는 메소드.
 
         Args:
             insert_value (Union[int, str, tuple[int, Any]]): [정수, 문자열, 우선 순위 Queue인 경우 (우선순위, 값)으로 입력.]
@@ -15,18 +15,18 @@ class QueueClass :
         self.queue.append(insert_value)
 
     def fifo_dequeue(self) -> Union[int, str] :
-        """FIFO 구조로 Queue 데이터를 꺼내는 함수."""
+        """FIFO 구조로 Queue 데이터를 꺼내는 메소드."""
         result = self.queue.pop(0)
         return result
 
     def lifo_dequeue(self) -> Union[int, str] :
-        """LIFO 구조로 Queue 데이터를 꺼내는 함수."""
+        """LIFO 구조로 Queue 데이터를 꺼내는 메소드."""
         result = self.queue.pop()
         return result
 
     def priority_dequeue(self) -> Union[int, str, tuple] :
         """우선순위 Queue.
-        사용자가 입력한 Tuple(우선순위, 값) 중 우선순위 숫자가 낮은 Queue 데이터를 꺼내는 함수.
+        사용자가 입력한 Tuple(우선순위, 값) 중 우선순위 숫자가 낮은 Queue 데이터를 꺼내는 메소드.
         """
         priority : list = []
 
