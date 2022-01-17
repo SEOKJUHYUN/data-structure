5efafe2261206e665b4447251e74c15f19e1b3f4
SEOKJUHYUN <seokjuhyun@naver.com>
2022-01-17 22:56:27 +0900

    Python Heap Data Structure

--- heap/heap.py
+++ heap/heap.py
@@ -1,10 +1,22 @@
 class MaxHeap :
-    def __init__(self, values) -> None:
+    """MaxHeap을 생성하는 Class이다.
+    생성자를 통해서 Root Node를 입력받고 개발할 때 편하기 위해서 0번 index를 None으로 사용한다.
+    """
+    def __init__(self, values: int) -> None:
         self.heap = list()
         self.heap.append(None)
         self.heap.append(values)
 
-    def cmp_max(self, index) :
+    def cmp_max(self, index: int) -> bool :
+        """입력받은 index 번호를 통해서 부모 index 번호를 계산하고 부모 Node와 값을 비교해서
+        자식 노드 값이 더 크면 True를 리턴한다.
+
+        Args:
+            index (int): [자식 Node의 Index 번호]
+
+        Returns:
+            bool: [자식 Node가 부모 Node 값보다 크면 True를 리턴한다.]
+        """
         result: bool = False
 
         parnet_index = index // 2
@@ -22,7 +34,15 @@ class MaxHeap :
         else :
             return result
 
-    def insert(self, values) :
+    def insert(self, values: int) -> None :
+        """Heap에 추가하고 싶은 값을 추가해주는 메소드이다.
+
+        Args:
+            values (int): [추가하고 싶은 값.]
+
+        Returns:
+            None: [별도의 리턴은 없다.]
+        """
         self.heap.append(values)
         index = self.heap.index(values)
 
@@ -31,17 +51,20 @@ class MaxHeap :
             self.heap[index], self.heap[parnet_index] = self.heap[parnet_index], self.heap[index]
             index = parnet_index
 
-        return True
+    def delete(self) -> int :
+        """MaxHeap Root Node를 삭제하는 메소드이다.
 
-    def delete(self) :
-        result = self.heap[1]
+        Returns:
+            int: [삭제된 값을 리턴한다.]
+        """
+        result: int = self.heap[1]
         self.heap[1] = self.heap[-1]
         del self.heap[-1]
         l_index = 2
         r_index = 3
 
         while True :
-            if l_index+r_index >= len(self.heap) * 2 -1 :
+            if l_index+r_index > len(self.heap) * 2 - 2 :
                 break
 
             if self.heap[l_index] > self.heap[r_index] :
@@ -65,12 +88,24 @@ class MaxHeap :
         return result
 
 class MinHeap :
-    def __init__(self, values) -> None:
+    """MinHeap을 생성하는 Class이다.
+    생성자를 통해서 Root Node를 입력받고 개발할 때 편하기 위해서 0번 index를 None으로 사용한다.
+    """
+    def __init__(self, values: int) -> None:
         self.heap = list()
         self.heap.append(None)
         self.heap.append(values)
 
-    def cmp_min(self, index) :
+    def cmp_min(self, index: int) -> bool :
+        """입력받은 index 번호를 통해서 부모 index 번호를 계산하고 부모 Node와 값을 비교해서
+        자식 노드 값이 더 작으면 True를 리턴한다.
+
+        Args:
+            index (int): [자식 Node의 Index 번호]
+
+        Returns:
+            bool: [자식 Node가 부모 Node 값보다 작으면 True를 리턴한다.]
+        """
         result: bool = False
 
         parnet_index = index // 2
@@ -88,7 +123,15 @@ class MinHeap :
         else :
             return result
 
-    def insert(self, values) :
+    def insert(self, values: int) -> None :
+        """Heap에 추가하고 싶은 값을 추가해주는 메소드이다.
+
+        Args:
+            values (int): [추가하고 싶은 값.]
+
+        Returns:
+            None: [별도의 리턴은 없다.]
+        """
         self.heap.append(values)
         index = self.heap.index(values)
 
@@ -97,9 +140,12 @@ class MinHeap :
             self.heap[index], self.heap[parnet_index] = self.heap[parnet_index], self.heap[index]
             index = parnet_index
 
-        return True
+    def delete(self) -> int :
+        """MinHeap Root Node를 삭제하는 메소드이다.
 
-    def delete(self) :
+        Returns:
+            int: [삭제된 값을 리턴한다.]
+        """
         result = self.heap[1]
         self.heap[1] = self.heap[-1]
         del self.heap[-1]
@@ -108,7 +154,7 @@ class MinHeap :
 
 
         while True :
-            if l_index+r_index >= len(self.heap) * 2 -1 :
+            if l_index+r_index > len(self.heap) * 2 - 2 :
                 break
 
             if self.heap[l_index] < self.heap[r_index] :
